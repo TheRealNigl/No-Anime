d3fba5ddcd5d35ebcbf34dd672b381ff26a7ceed
mguhf <41440935+mguhf@users.noreply.github.com>
2018-07-26 11:37:10 -0500

    awesomness from ALex damarjian

---
+++ No Anime/Assets/LoadLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+
+    // Use this for initialization
+    public string levelName;
+	void Start () {
+        StartCoroutine("Wait");
+
+
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        Application.LoadLevel(levelName);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
